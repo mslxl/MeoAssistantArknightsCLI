4ee14688757c1836dd17bc0d30a985b6adf48541
mslxl <i@mslxl.com>
2022-02-08 00:09:35 +0800

    fix: ld 链接错误

--- tools/build.sh
+++ tools/build.sh
@@ -96,12 +96,5 @@ if [[ -d "$pkg_root/../lib" ]]; then
   rm -rf "$pkg_root/../lib"
 fi
 
-cat > "$pkg_root/build/maa" <<\EOF
-#!/bin/bash
-LD_LIBRARY_PATH=. ./MeoAssistantArknightsCLI "$@"
-EOF
-
-chmod a+x "$pkg_root/build/maa"
-
 cp -r "$pkg_root/build" "$pkg_root/../lib"
 
